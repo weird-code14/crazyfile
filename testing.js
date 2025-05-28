55aff79f1788c1847d43bdc31883994a284b5e31
weird-code14 <alisondavid0168@gmail.com>
2025-05-28 07:29:13 -0700

    my teacher
    he makes the class interestin

---
+++ testing.js
@@ -0,0 +1,15 @@
+
+// let x = 8
+// let y = 9
+
+// alert(x*y)
+let damage = 15
+let health = 20
+let vestdurability = 10
+let totalhealth = 30
+
+if(totalhealth - damage > 15 ) {
+    alert("you are dead")
+
+}else( totalhealth - damage <= 15) 
+    alert("you are alive with " + 15)
